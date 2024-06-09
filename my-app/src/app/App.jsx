0706151323f1b58ac9e576dd31ef553529b55769
MarkHallett <git@markhallett.co.uk>
2024-06-09 17:37:32 +0100

    Create App.jsx
    Initial save

---
+++ my-app/src/app/App.jsx
@@ -0,0 +1,203 @@
+'use client'
+import axios from 'axios'
+import useWebSocket from 'react-use-websocket';
+import { Graphviz } from 'graphviz-react';
+
+import { useState, useEffect } from 'react'
+
+import { Form, Button } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+
+const SOCKET_URL_ONE = 'ws://localhost:8000/ws/123';
+const READY_STATE_OPEN = 1;
+
+
+const GraphvizPage = (xx) => {
+  if (Object.is(xx, null)) { 
+       return 'no connection';
+  } else {
+       console.log('use: ... ')
+       console.log( xx)
+       console.log( 'done')
+       return <Graphviz dot={xx}/>;
+  }
+}
+
+//Generates the click handler, which returns a promise that resovles to the provided url.
+const generateAsyncUrlGetter =
+  (url, timeout = 2000) =>
+  () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(url);
+      }, timeout);
+    });
+  };
+
+
+const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Send node/value', event.target.id, event.target.value)
+
+      if (event.target.id === 'nodeA') {
+        axios.patch('http://localhost:8000/items/gbp-usd?value=' + event.target.value ,
+         { headers: { 'Content-Type': 'application/json; charset=utf-8', } })
+      }
+
+      if (event.target.id === 'nodeB') {
+        axios.patch('http://localhost:8000/items/usd-eur?value=' + event.target.value ,
+         { headers: { 'Content-Type': 'application/json; charset=utf-8', } })
+      }
+
+      if (event.target.id === 'nodeC') {
+        axios.patch('http://localhost:8000/items/eur-gbp?value=' + event.target.value ,
+         { headers: { 'Content-Type': 'application/json; charset=utf-8', } })
+      }
+    }
+  }
+
+function App() {
+
+  const [currentSocketUrl, setCurrentSocketUrl] = useState(null);
+  const [messageHistory, setMessageHistory] = useState(null);
+  const { sendMessage, lastMessage, readyState, getWebSocket } = useWebSocket(
+    currentSocketUrl,
+    {
+      share: true,
+      shouldReconnect: () => false,
+    }
+  );
+
+  useEffect(() => {
+    lastMessage && setMessageHistory(lastMessage.data);
+  }, [lastMessage]);
+
+  const readyStateString = {
+    0: 'CONNECTING',
+    1: 'OPEN',
+    2: 'CLOSING',
+    3: 'CLOSED',
+  }[readyState];
+
+
+  const [toggleStatus, setToggleStatus] = useState(true)
+  const [nodeValue, setNodeValue] = useState(null)
+
+  const sendValue= (e) => {
+    console.log("SEND SINGLE NODE VALUE")
+  }
+
+  const doSubmit= (e) => {
+    e.preventDefault();
+    console.log('Send all node values, nodeA', nodeA, 'nodeB', nodeB, 'nodeC', nodeC)
+
+    axios.patch('http://localhost:8000/items/gbp-usd?value=' + nodeA ,
+    { headers: { 'Content-Type': 'application/json; charset=utf-8', } }
+    )
+
+    axios.patch('http://localhost:8000/items/usd-eur?value=' + nodeB ,
+    { headers: { 'Content-Type': 'application/json; charset=utf-8', } }
+    )
+
+    axios.patch('http://localhost:8000/items/eur-gbp?value=' + nodeC ,
+    { headers: { 'Content-Type': 'application/json; charset=utf-8', } }
+    )
+  }
+
+const [nodeA, setNodeA] = useState(0);
+const [nodeB, setNodeB] = useState(0);
+const [nodeC, setNodeC] = useState(0);
+
+return ( 
+    <>
+    <Container>
+      <p></p>
+      <h1>pydagoras</h1>
+      <p>Basic example</p>
+      <br />
+        <Row xs={2} md={4} lg={6}>
+          <Col>Backend websocket</Col>
+          <Col>{SOCKET_URL_ONE}</Col>
+        </Row>
+        <Row xs={2} md={4} lg={6}>
+          <Col></Col>
+          <Col> <button type="button" 
+               class="btn btn-primary me-3" 
+               data-bs-toggle="connect" 
+               data-bs-target="#offcanvasExample"
+               onClick={() => setCurrentSocketUrl(generateAsyncUrlGetter(SOCKET_URL_ONE)) }
+               disabled={currentSocketUrl === SOCKET_URL_ONE}
+               >Connect</button>
+          </Col>
+        </Row>
+        <Row xs={2} md={4} lg={6}>
+          <Col>Connection Status</Col>
+          <Col> {readyStateString} </Col>
+        </Row>
+        <p></p>
+        <Row xs={2} md={4} lg={6}>
+          <Col>gbp-usd</Col>
+          <Col> <Form.Control 
+                    id="nodeA"
+                    type="number" 
+                    value={nodeValue}
+                    placeholder="Node value" 
+                    onKeyDown={handleKeyDown}
+                    onChange={(e) => setNodeA(e.target.value)}
+                /> 
+            </Col>
+        </Row>
+
+      <Row xs={2} md={4} lg={6}>
+        <Col>usd-eur</Col>
+        <Col> <Form.Control 
+                    id="nodeB"
+                    type="number" 
+                    value={nodeValue}
+                    placeholder="Node value" 
+                    onKeyDown={handleKeyDown}
+                    onChange={(e) => setNodeB(e.target.value)}
+              /> 
+        </Col>
+      </Row>
+
+      <Row xs={2} md={4} lg={6}>
+        <Col>eur-gbp</Col>
+        <Col> <Form.Control 
+                    id="nodeC"
+                    type="number" 
+                    value={nodeValue}
+                    placeholder="Node value" 
+                    onKeyDown={handleKeyDown}
+                    onChange={(e) => setNodeC(e.target.value)}
+        /> 
+        </Col>
+      </Row>
+
+      <Row xs={2} md={4} lg={6}>
+        <Col> </Col>
+        <Col> 
+          <Form.Check
+              type="switch"
+              label= {toggleStatus ? "Send individually" : "Send all"} 
+              onClick = {() => setToggleStatus(!toggleStatus)}
+          />
+        </Col>
+      </Row>
+
+      <Row xs={2} md={4} lg={6}>
+        <Col> </Col>
+        <Col> <Button variant="primary" type="submit" disabled={toggleStatus} onClick={doSubmit}> Submit </Button> </Col>
+      </Row>
+
+      {GraphvizPage(messageHistory)}
+
+    </Container>
+    </>
+  )
+}
+
+export default App
