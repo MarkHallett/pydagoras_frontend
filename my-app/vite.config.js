88a1bea76e35849c6d4275decb816e074d1dd380
Mark Hallett <pydagoras@markhallett.co.uk>
2025-07-08 13:34:03 +0100

    big clean

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -1,16 +1,32 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+//https://vite.dev/config/
+//         #cert: '/www/www_pydagoras_com.crt',
+//         #key: '/www/www_pydagoras_com.key'
 export default defineConfig({
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        nested: resolve(__dirname, 'nested/index.html'),
+      },
+    },
+  },
+
+
+
   plugins: [react()],
 
      server: {
        port: 443,
-       https: {
-         cert: '/www/www_pydagoras_com.crt',
-         key: '/www/www_pydagoras_com.key'
-       }
+//       https: {
+//         cert: 'certs/www_pydagoras_com.crt',
+//         key: 'certs/www_pydagoras_com.key'
+//       }
      }
 
 })
