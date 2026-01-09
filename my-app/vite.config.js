70b14e167b328f66d3d49eda469a3f8358410d78
Mark Hallett <pydagoras@markhallett.co.uk>
2026-01-09 17:07:11 +0000

    update for prod

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,16 +3,12 @@ import { fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-//https://vite.dev/config/
-//         #cert: '/www/www_pydagoras_com.crt',
-//         #key: '/www/www_pydagoras_com.key'
 export default defineConfig({
 
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        nested: resolve(__dirname, 'nested/index.html'),
       },
     },
   },
